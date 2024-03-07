34f9fc64e4266d167f865538708698645c3d7595
Kaio Da Dalt <kaiodadalt@gmail.com>
2024-03-07 06:52:04 -0300

    Feat: added qodana and solved some problems reported.

--- Programming Language 1 - Python/Class 1/1b - More Python/exercise1.py
+++ Programming Language 1 - Python/Class 1/1b - More Python/exercise1.py
@@ -3,7 +3,8 @@
 
     Refaça o exercício da fórmula de Bhaskara da seguinte forma:
     
-    a. Com quatro raízes de saída (x1_0 e x2_0, com o resultado real, e x1_1 e x2_1 com resultado arredondado para baixo);
+    a. Com quatro raízes de saída (x1_0 e x2_0, com o resultado real,
+        e x1_1 e x2_1 com resultado arredondado para baixo);
     b. Use funções do módulo math, sempre que possível;
     c. Calcule o erro resultante do arredondamento.
 """
--- Programming Language 1 - Python/Class 1/1b - More Python/exercise3.py
+++ Programming Language 1 - Python/Class 1/1b - More Python/exercise3.py
@@ -1,15 +1,19 @@
 """
     *** EXERCISE 3 ***
 
-    Qual o resultado da expressão "23+7*50" ?
+    Qual o resultado da expressão "23 + 7 * 50" ?
 
     a. Como obter o resultado 1500 da expressão acima? Escreva essa instrução em Python.
 """
 
+# Calculando o resultado da expressão original
 resultado = 23 + 7 * 50
 
-print(f"O resultado da expressão '23 + 7 * 50' é {resultado}")
+# Imprimindo o resultado da expressão original
+print(f"O resultado da expressão 23 + 7 * 50 é {resultado}")
 
+# Calculando o resultado da expressão modificada
 resultado_modificado = (23 + 7) * 50
 
+# Imprimindo o resultado da expressão modificada
 print(f"O resultado da expressão '(23 + 7) * 50' é {resultado_modificado}")

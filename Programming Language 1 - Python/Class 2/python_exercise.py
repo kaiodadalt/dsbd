20d359a8e2e88b3cba2059c47998d3021c5d20d6
Kaio Freitas Da Dalt <kfdalt@inf.ufpr.br>
2024-03-15 21:53:15 -0300

    Feat: get header dynamically.

--- Programming Language 1 - Python/Class 2/python_exercise.py
+++ Programming Language 1 - Python/Class 2/python_exercise.py
@@ -1,14 +1,18 @@
-header = 'date(mmddyyyy),srcIP,srcPort,dstIP,dstPort,protocol,priority,IDSID,SignatureGroup,Classification\n'
+header = ''
 fixed_content_lines = []
 
 with open('python_exercise.csv') as content:
-    for row in content:
+    for index, row in enumerate(content):
         row_data = row.strip().split(',')
         fixed_row = []
 
         for column in row_data:
             if ':' in column:
                 key, value = column.split(':', 1)
+
+                if index == 1:
+                    header += key + ','
+
                 fixed_row.append(value)
             else:
                 fixed_row.append('')
@@ -16,5 +20,4 @@ with open('python_exercise.csv') as content:
         fixed_content_lines.append(','.join(fixed_row))
 
 fixed_content = '\n'.join(fixed_content_lines)
-
-print(header + fixed_content)
+print(header[:-1] + fixed_content)
